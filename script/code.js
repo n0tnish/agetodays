4478b76865a39bbc548da4add3f558b24a34dd6f
n0tnish <nishaatgafieldien@gmail.com>
2023-11-23 16:58:38 +0200

    added

---
+++ script/code.js
@@ -0,0 +1,14 @@
+const dateInput = document.querySelector('input');
+const pElement = document.querySelector('p');
+
+dateInput.addEventListener('change', ( ) => {
+    const birthDate = new Date(dateInput.value);
+    const currenDate = new Date ( );
+    const newDate = new Date( currenDate - birthDate);
+
+    const years = newDate.getFullYear( ) - 1970;
+    const months = newDate.getMonth( );
+    const days = newDate.getDate( ) -1;
+
+    pElement.innerText = `You are ${years} years ${months} months ${days} days old.` ;
+})
